4d1b96b9c7dcfa27fb934279b142b5c78cc34adf
Kim <kim_pham@berkeley.edu>
2020-07-19 21:23:00 -0700

    "connecting to GitHub"

--- miniProject1/ViewController.swift
+++ miniProject1/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var randomFact: UILabel!
+    
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func clicked(_ sender: UIButton) {
+        
+        var facts = ["I love ice cream!", "I love hiking", "I train in Taekwondo", "My favorite movie is The Martian"]
+        var randomInt = Int.random(in: 0..<4)
+        randomFact.text = facts[randomInt]
+        
+        //randomFact.text = "I love ice cream"
+        
+    }
+    
 }
 
